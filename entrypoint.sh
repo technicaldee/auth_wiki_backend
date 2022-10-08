5b0cfd79bf6a83af0fad718772398fc5d62321d7
Edidiong Udoh <42146638+technicaldee@users.noreply.github.com>
2022-10-08 18:22:33 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,6 @@
-web: gunicorn core.wsgi --log-file -
+#!/bin/shell
+
+python manage.py migrate  --no-input
+python manage.py collectstatic --no-input
+
+gunicorn core.asgi:application --bind 0.0.0.0:8000 -w 2 -k uvicorn.workers.UvicornWorker
